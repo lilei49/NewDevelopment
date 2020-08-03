ec7a4b1bec95f1b483a2609e860a70e3659070e7
李磊 <15271839945@163.com>
2020-08-03 19:24:54 +0800

    编写部分代码

---
+++ Development/Development.DBHelper/AbstractDBHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Development.DBHelper
+{
+    public class AbstractDBHelper
+    {
+        private SqlConnection SqlConnection = new SqlConnection();
+
+        void Open() {
+            try
+            {
+                if (SqlConnection.State == ConnectionState.Closed)
+                {
+                    SqlConnection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                Close();
+                Message_Util
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        void Close()
+        { 
+        
+        }
+    }
+}
---
+++ Development/Development.Util/Connection_Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Development.Util
+{
+    public class Connection_Util
+    {
+        private static System.String _connString;
+        public static System.String ConnString
+        {
+            get { return _connString; }
+            set { _connString = value; }
+        }
+
+        private static System.String _database = "DevelopmentDB";
+        public static System.String DataBase
+        {
+            get { return _database; }
+            set { _database = value; }
+        }
+    }
+}
